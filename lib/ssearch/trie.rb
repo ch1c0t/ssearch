d750428143730cf642b965585fab998ff077240c
Anatoly Chernow <ch1c0t@yahoo.com>
2013-10-17 16:41:26 +0000

    Return array of node strings with count of its occurences

--- lib/ssearch/trie.rb
+++ lib/ssearch/trie.rb
@@ -3,15 +3,23 @@ class Ssearch
     def initialize port, ngram_size
       @dbs = []
       ngram_size.times { |size| @dbs << Redis.new(port: port, db: size) }
+
+      @nodes = Redis.new port: port, db: 15
     end
 
-    def prefix string
-      response = []
-      @dbs.each do |db|
-        response = response + db.keys(string + "*")
+    def prefix string, size: 7
+      unless @nodes.exists string
+        @dbs.each do |db|
+          autocompletions = db.keys(string + "*")
+          autocompletions.each do |autocompletion|
+            @nodes.zadd string, db.scard(autocompletion), autocompletion
+          end
+        end
       end
 
-      response
+      @nodes.zrevrange(string, 0, size, with_scores: true).map do |(string, cardinality)|
+        [string, cardinality.to_i]
+      end
     end
   end
 end
--- spec/trie_spec.rb
+++ spec/trie_spec.rb
@@ -4,9 +4,17 @@ trie = seed_test_ssearch.trie
 
 describe Ssearch::Trie do
   describe '#prefix' do
-    it 'returns array of node strings' do
-      expected = ["above", "able", "above .", "able to", "able to classify", "able to build", "able to classify new", "able to build systems"]
-      trie.prefix('ab').must_equal expected
+    it 'returns array of node strings with count of its occurances' do
+      trie.prefix('a').must_equal [
+                                    ["a", 16], 
+                                    ["and", 15], 
+                                    ["as", 7], 
+                                    ["also", 5], 
+                                    ["an", 4], 
+                                    ["a set of", 3], 
+                                    ["at", 3], 
+                                    ["a set", 3]
+                                  ]
     end
   end
 end
