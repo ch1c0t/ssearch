b5b327d4eca91b9c9a45f51b9753a82ce379b4d1
Anatoly Chernow <ch1c0t@yahoo.com>
2013-10-20 09:35:51 +0000

    Create trie node in private method

--- lib/ssearch/trie.rb
+++ lib/ssearch/trie.rb
@@ -8,17 +8,21 @@ class Ssearch
     end
 
     def prefix string, size: 7
-      unless @nodes.exists string
-        @dbs.each do |db|
-          autocompletions = db.keys(string + "*")
-          autocompletions.each do |autocompletion|
-            @nodes.zadd string, db.scard(autocompletion), autocompletion
-          end
-        end
-      end
+      create_node_for string unless @nodes.exists string
 
       @nodes.zrevrange(string, 0, size, with_scores: true).map do |(string, cardinality)|
         [string, cardinality.to_i]
+      end
+    end
+
+    private
+
+    def create_node_for string
+      @dbs.each do |db|
+        autocompletions = db.keys(string + "*")
+        autocompletions.each do |autocompletion|
+          @nodes.zadd string, db.scard(autocompletion), autocompletion
+        end
       end
     end
   end
